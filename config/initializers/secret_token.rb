4a71f523b4ce7f6111316498bde4a08f0dd6614c
Aaron Dufall <aald212@gmail.com>
2012-07-25 19:20:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buildcloud::Application.config.secret_token = '447b3ab0b84d1cdd566cd769e5020cf1baf2ff08ac24d8c01fe0e6f670ba1912ed3da709720baf890e2a1a353da3134528477886ae6030d36d4a40bde406dfe7'
