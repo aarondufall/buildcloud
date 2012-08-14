cbf4a49d4591d1613f7312832b10bc0e7f61ec8a
Aaron Dufall <aald212@gmail.com>
2012-08-14 21:52:15 +0800

    Add edit update and destroy to task list

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -1,4 +1,6 @@
 class TasklistsController < ApplicationController
+	respond_to :html
+
 	def index
 		@project = current_project(params[:project_id])
 		@tasklists = @project.tasklists
@@ -16,6 +18,32 @@ class TasklistsController < ApplicationController
 		else
 			render 'new'
 		end	
+	end
+
+	def edit
+		@tasklist = Tasklist.find(params[:id])
+		respond_with @tasklist
+	end	
+
+	def update
+		@tasklist = Tasklist.find(params[:id])
+		if @tasklist.update_attributes(params[:tasklist])
+			flash[:success] = "List sucessfully updated"
+			redirect_to project_path(@tasklist.project)
+		else
+			flash[:error] = "List failed to update"
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if Tasklist.find(params[:id]).destroy
+    		flash[:success] = "List deleted."
+    		redirect_to project_path(current_project(params[:project_id]))
+    	else
+    		flash[:error] = "List failed to delete"
+    		redirect_to project_path(current_project(params[:project_id]))
+    	end
 	end
 
 	private
