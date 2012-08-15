1d06fc88fc6e115c726e7e180ceb6dc045d974a9
Darcy Laycock <sutto@sutto.net>
2012-08-15 20:10:03 +0800

    User has many projects via their teams.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  
+
   # attr_accessible :title, :body
 
   has_and_belongs_to_many :teams
+
+  has_many :projects, :through => :teams
+
   has_one :owned_team, :class_name => "Team", :foreign_key => 'user_id', :dependent => :destroy
-  
+
   has_one :profile
   accepts_nested_attributes_for :profile
-  
+
   attr_accessible :email, :password, :password_confirmation, :remember_me, :profile_attributes
 
   searchable do
@@ -29,12 +32,12 @@ class User < ActiveRecord::Base
 
 
   private
-    
+
 
 
     def set_user_on_team
       self.create_owned_team(:name => self.profile.name ).users << self
 
     end
- 
+
 end
