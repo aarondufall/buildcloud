69739d7cfbcf46bb359251aa874ffe305f1f2754
Aaron Dufall <aald212@gmail.com>
2012-08-16 21:40:38 +0800

    new task form - work in progress

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,8 @@
 class TasksController < ApplicationController
+
+	def new
+	  @project = current_project
+	  @tasklist = @project.tasklists.find(params[:tasklist_id])
+	  @task = @tasklist.tasks.build	
+	end
 end
