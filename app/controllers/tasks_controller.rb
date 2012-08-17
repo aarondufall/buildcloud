04f5240a46553dc5817240fefb1894110356c933
Aaron Dufall <aald212@gmail.com>
2012-08-17 10:55:54 +0800

    refactored tasklist scope DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,20 @@ class TasksController < ApplicationController
 
 	def new
 	  @project = current_project
-	  @tasklist = @project.tasklists.find(params[:tasklist_id])
+	  @tasklist = current_tasklists.find(params[:tasklist_id])
 	  @task = @tasklist.tasks.build	
+	end
+
+	def create
+		@project = current_project
+		@tasklist = current_tasklists.find(params[:tasklist_id])
+		@task = @tasklist.tasks.build(params[:task])
+		if @task.save
+			flash[:success] = "Added new task to #{@tasklist.name} created"
+			redirect_to current_project
+		else
+			flash[:error] = "Failed"
+			render 'new'
+		end
 	end
 end
