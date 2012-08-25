a97ad37d53dbbdbbd27989dd3d16499a24db6409
Darcy Laycock <sutto@sutto.net>
2012-08-25 13:45:15 +0800

    Introduce a render_shared_menu helper, relative dates and todo list end date support

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,27 @@
 module ProjectsHelper
+
+  def render_project_menu(project)
+    render partial: 'shared/menu', locals: {project: project}
+  end
+
+  def formatted_todolist_end_date(todolist)
+    date          = todolist.end_date
+    duration_text = relative_date(date, Time.now.to_date)
+    output        = ActiveSupport::SafeBuffer.new
+    if date.future?
+      output << content_tag(:span, duration_text, :class => "date")
+      output << " Until schedule completion date"
+    else
+      output << "Was due for completion"
+      output << content_tag(:span, duration_text, :class => "date overdue")
+      output << " ago"
+    end
+    output
+  end
+
+  def relative_date(from, relative_to = Time.now)
+    duration_text = distance_of_time_in_words relative_to, from
+    content_tag :abbr, duration_text, :class => "relative-date", :title => l(from, :format => :long)
+  end
+
 end
