196e15510b86b9c3dd2553992df27c4cd15f8395
Aaron Dufall <aald212@gmail.com>
2012-08-28 21:19:07 +0800

    Team has many Contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   attr_accessible :address, :company, :email, :first_name, :last_name, :phone
+  belongs_to :team
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -4,6 +4,7 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :users, :before_add => :validates_user
   belongs_to :owner, :class_name => "Account", :foreign_key => 'account_id'
   has_many :projects
+  has_many :contacts
 
   def validates_user(user)
   	raise ActiveRecord::Rollback if self.users.include? user
