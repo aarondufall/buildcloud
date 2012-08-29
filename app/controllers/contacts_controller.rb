32f2f704b2083e990bcf1ea60d708a3ff5105153
Aaron Dufall <aald212@gmail.com>
2012-08-29 13:01:49 +0800

    create contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,19 @@ class ContactsController < ApplicationController
 	def index
 		@contacts = current_team.contacts	
 	end
+
+	def new
+		@contact = current_team.contacts.build
+	end
+
+	def create
+		@contact = current_team.contacts.build(params[:contact])
+		if @contact.save
+			flash[:success] = 'New Contact Added'
+			redirect_to [current_team, :contacts]
+		else
+			flash[:error] = 'Fails to Add Contact'
+			render 'new'
+		end
+	end
 end
