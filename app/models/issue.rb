5cacee17b2fe26c596ba38de487d48da5dba368a
Aaron Dufall <aald212@gmail.com>
2012-09-02 19:33:47 +0800

    added user and project associations

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,7 @@
 class Issue < ActiveRecord::Base
   attr_accessible :assigned_to_id, :closed_at, :description, :title, :todo_id, :type
   belongs_to :todo
+  belongs_to :user, :class_name => "User", :foreign_key => 'assigned_to_id'
+  belongs_to :project
+  validates :title, :presence => true
 end
