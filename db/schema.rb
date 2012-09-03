f5d546a0ed40c8c222ad3e864b326e1d18f0dbfb
Aaron Dufall <aald212@gmail.com>
2012-09-03 16:44:47 +0800

    updated issues table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120901023112) do
+ActiveRecord::Schema.define(:version => 20120903060815) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -37,13 +37,14 @@ ActiveRecord::Schema.define(:version => 20120901023112) do
   create_table "issues", :force => true do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "type"
+    t.string   "issue_type"
     t.integer  "assigned_to_id"
     t.datetime "closed_at"
     t.integer  "todo_id"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.integer  "project_id"
+    t.integer  "created_by_id"
   end
 
   create_table "profiles", :force => true do |t|
