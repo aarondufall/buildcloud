57ee7209144bc05adc775f65b03447e4ab1fc363
Aaron Dufall <aald212@gmail.com>
2012-10-08 16:25:10 +0800

    has_many follow_ups

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :teams
   has_many :projects, :through => :teams
+  has_many :follow_ups
   has_one :account, :dependent => :destroy
 
 
