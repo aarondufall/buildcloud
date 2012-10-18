06e4d3992d6f02b452a3e1592355c066bdd435f1
Aaron Dufall <aald212@gmail.com>
2012-10-19 06:22:37 +0800

    registration experiments

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,22 +8,26 @@ class RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+  	@invite = Invite.find(session[:invite]) if session[:invite]
     build_resource
+    session[:invite] = nil
+    flash[:notice] = "#{resource} Params: #{params} Invite:#{@invite}"
 
-    if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
-    end
+    redirect_to :root
+    #if resource.save
+    #  if resource.active_for_authentication?
+    #    set_flash_message :notice, :signed_up if is_navigational_format?
+    #    sign_in(resource_name, resource)
+    #    respond_with resource, :location => after_sign_up_path_for(resource)
+    #  else
+    #    set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+    #    expire_session_data_after_sign_in!
+    #    respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+    #  end
+    #else
+    #  clean_up_passwords resource
+    #  respond_with resource
+    #end
   end
 
 end
