0bbb4b7dacc20c43d19343fe6890bca3381b9b37
Aaron Dufall <aald212@gmail.com>
2012-10-29 21:39:16 +0800

    Invite logic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,30 +1,46 @@
 class RegistrationsController < Devise::RegistrationsController
+
 	def new
-     @invite = Invite.find(session[:invite]) if session[:invite]
-     resource = build_resource({})
-     respond_with resource
+    if session[:invite] 
+      @invite = Invite.find(session[:invite])
+    end 
+    resource = build_resource({})
+    respond_with resource
   end
 
   # POST /resource
   def create
-  	if session[:invite]
+
+    build_resource
+    if session[:invite] 
       @invite = Invite.find(session[:invite])
-      @user = @invite.account.users.build(params[:user])
+      @account = @invite.account
     else
-      @user = User.new(params[:user])
-      @account = @user.build_account
+      @account = resource.build_account
       @account.name = params[:user][:profile_attributes][:company]
     end
-    if @user.save
-      @account.give_user_access(@user)
-      @user.active_for_authentication?
-      set_flash_message :notice, :signed_up if is_navigational_format?
-      sign_in(resource_name, @user)
-      respond_with @user, :location => after_sign_up_path_for(@user)
+    if resource.save
+      @account.give_user_access(resource)
+      add_to_project(@invite, @user) unless false
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
     else
       clean_up_passwords resource
       respond_with resource
     end
   end
 
+  private
+
+    def add_to_project(invite, user)
+      return false unless invite != nil
+      invite.accept_as(user)
+    end
 end
